35a56a856830f7e8fa13d1c0a8f0a2d33de7d716
Divi-Adiffia-Freza-Alana <55723716+Divi-Adiffia-Freza-Alana@users.noreply.github.com>
2019-09-24 13:44:29 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118084/latihan12/aritmatika/PBO210118084Latihan12Aritmatika.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118084.latihan12.aritmatika;
+
+/**
+ *
+ * @author
+ * Nama  : Divi Adiffia Freza Alana
+ * Kelas : PBO2
+ * Nim   : 10118084
+ * Deskripsi Program : Program ini berisi program untuk menampilkan Aritmatika
+
+ */
+public class PBO210118084Latihan12Aritmatika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10;
+        int b = 20;
+        int c = 25; 
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+        
+        
+    }
+    
+}
